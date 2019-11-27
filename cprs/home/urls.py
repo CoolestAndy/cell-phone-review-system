76b714c1d1338a7da9b5c15d575d6db9ef0628db
Haotian Wang <haotian-wang@foxmail.com>
2019-11-27 18:27:51 -0500

    Home Page and Index Page

--- cprs/home/urls.py
+++ cprs/home/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.HomeView.as_view()),
+    path('', views.HomeView.as_view(), name='home'),
     path('sign-in', views.SignInView.as_view()),
-    path('details', views.DetailsView.as_view()),
+    path('details/<str:asin>', views.DetailsView.as_view(), name='details'),
 ]
--- cprs/home/views.py
+++ cprs/home/views.py
@@ -1,11 +1,17 @@
+from collections import Counter
 from django.shortcuts import render
 from django.views import View
+from .models import *
 
 class HomeView(View):
     template = 'home/index.html'
 
     def get(self, request):
-        return render(request, self.template)
+        ctx = {
+            'brands': Brand.objects.all(),
+            'items': Item.objects.all()
+        }
+        return render(request, self.template, ctx)
 
 class SignInView(View):
     template = 'home/sign-in.html'
@@ -16,5 +22,21 @@ class SignInView(View):
 class DetailsView(View):
     template = 'home/details.html'
 
-    def get(self, request):
-        return render(request, self.template)
+    def get(self, request, asin):
+        item = Item.objects.get(asin=asin)
+        reviews = Review.objects.filter(item=item)
+        rating_count = Counter(item.ratings.all())
+        rating_count = { rating.rating: count for rating, count in rating_count.items()}
+        ctx = {
+            'ratings': {
+                'ticks': [0.5, 1.5, 2.5, 3.5, 4.5],
+                'count': rating_count,
+                'percentage': {
+                    rating: '{}%'.format(count * 100 / max(rating_count.values()))
+                    for rating, count in rating_count.items()
+                }
+            },
+            'item': item,
+            'reviews': reviews
+        }
+        return render(request, self.template, ctx)
