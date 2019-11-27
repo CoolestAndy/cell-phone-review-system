814fdc25d36cc9bb68a1a9ca371892e075d03b53
Haotian Wang <haotian-wang@foxmail.com>
2019-11-27 11:35:59 -0500

    Change dataset

--- cprs/home/models.py
+++ cprs/home/models.py
@@ -1,18 +1,10 @@
-from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
 
 
-class Manufacturer(models.Model):
-    name = models.CharField(max_length=200)
-
-
-class Language(models.Model):
-    name = models.CharField(max_length=20)
-
-
-class Country(models.Model):
+class Brand(models.Model):
     name = models.CharField(max_length=50)
 
 
@@ -22,19 +14,29 @@ class Rating(models.Model):
     )
 
 
-class Phone(models.Model):
-    name = models.CharField(max_length=200)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    rating = models.ManyToManyField(Rating, through='Review')
-    # base64 encoded image data
-    image = models.TextField()
+class Carrier(models.Model):
+    CANDIDATES = ['Unlocked', 'AT&T', 'T-Mobile', 'Mint', 'Verizon', 'Sprint', 'MetroPCS', 'Cricket', 'H2O']
+    name = models.CharField(max_length=10)
+
+
+class Item(models.Model):
+    asin = models.CharField(primary_key=True, max_length=10)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    carrier = models.ForeignKey(Carrier, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    image = models.URLField()
+    ratings = models.ManyToManyField(Rating, through='Review')
+    average_rating = models.DecimalField(max_digits=2, decimal_places=1)
+    total_reviews = models.IntegerField()
+    min_price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    max_price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
 
 
 class Review(models.Model):
-    content = models.TextField()
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    time = models.DateTimeField(auto_now=True)
-    language = models.ForeignKey(Language, on_delete=models.CASCADE)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
     rating = models.ForeignKey(Rating, on_delete=models.CASCADE)
-    phone = models.ForeignKey(Phone, on_delete=models.CASCADE)
+    date = models.DateField()
+    title = models.TextField()
+    body = models.TextField()
+    helpful_votes = models.IntegerField()
--- cprs/scripts/load_data.py
+++ cprs/scripts/load_data.py
@@ -7,37 +7,46 @@ from home.models import *
 
 
 def run():
-    Manufacturer.objects.all().delete()
-    Language.objects.all().delete()
+    Brand.objects.all().delete()
     Rating.objects.all().delete()
+    Carrier.objects.all().delete()
     User.objects.all().delete()
-    Country.objects.all().delete()
-    Phone.objects.all().delete()
+    Item.objects.all().delete()
     Review.objects.all().delete()
 
-    with open('data/phone_images.json', 'r', encoding='utf-8') as f:
-        images = json.load(f)
-    with open('data/phone_user_review_file.csv', 'r', encoding='utf-8') as f:
+    with open('data/items.csv', 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         next(reader)
-        for row in tqdm(reader, total=1415134):
-            try:
-                manufacturer, _ = Manufacturer.objects.all().get_or_create(name=row[4])
-                language, _ = Language.objects.all().get_or_create(name=row[2])
-                country, _ = Country.objects.all().get_or_create(name=row[3])
-                rating, _ = Rating.objects.all().get_or_create(rating=5)
-                image = images[row[0].split('/')[2]]
-                author, _ = User.objects.all().get_or_create(username=row[9])
-                phone, _ = Phone.objects.all().get_or_create(name=row[10], manufacturer=manufacturer, image=image)
-                Review.objects.all().create(
-                    content=row[8],
-                    author=author,
-                    time=datetime.strptime(row[1], '%m/%d/%Y'),
-                    language=language,
-                    country=country,
-                    rating=rating,
-                    phone=phone
-                )
-            except Exception as e:
-                print("Failed to create object for", row)
-                print(e)
+        for row in tqdm(reader, total=792):
+            brand, _ = Brand.objects.get_or_create(name=row[1])
+            carriers = [carrier for carrier in Carrier.CANDIDATES if carrier.replace('-', '').lower() in row[2].replace('-', '').lower()]
+            carrier, _ = Carrier.objects.get_or_create(name=carriers[0] if carriers else 'Unlocked')
+            prices = [float(price.replace('$', '')) for price in row[8].split(',') if price]
+            Item.objects.create(
+                asin=row[0],
+                brand=brand,
+                carrier=carrier,
+                title=row[2],
+                image=row[4],
+                average_rating=row[5],
+                total_reviews=row[7],
+                min_price=min(prices) if prices else None,
+                max_price=max(prices) if prices else None
+            )
+
+    with open('data/reviews.csv', 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in tqdm(reader, total=82794):
+            item = Item.objects.get(asin=row[0])
+            author, _ = User.objects.get_or_create(username=row[1])
+            rating, _ = Rating.objects.get_or_create(rating=int(row[2]))
+            Review.objects.create(
+                item=item,
+                author=author,
+                rating=rating,
+                date=datetime.strptime(row[3], '%B %d, %Y'),
+                title=row[5],
+                body=row[6],
+                helpful_votes=int(row[7]) if row[7] else 0
+            )
