3dac60e52c3b1ad8c358f4561f478934313ad126
Haotian Wang <haotian-wang@foxmail.com>
2019-11-28 23:29:08 -0500

    Sort options

--- cprs/home/views.py
+++ cprs/home/views.py
@@ -1,39 +1,74 @@
 from collections import Counter
 from datetime import datetime
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
-from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import *
+
 
 class HomeView(View):
     template = 'home/index.html'
 
+    def search_keywords(self, document, query):
+        document = document.lower()
+        for keyword in query.split(' '):
+            if keyword.lower() not in document:
+                return False
+        return True
+
+    def filter_items(self, items, filters):
+        filtered_items = []
+        for item in items:
+            if filters['keywords'] and not self.search_keywords(item.title, filters['keywords']):
+                continue
+            if filters['brand'] and item.brand.name != filters['brand']:
+                continue
+            if filters['carrier'] and item.carrier.name != filters['carrier']:
+                continue
+            if filters['min_price'] and (item.max_price is None or item.max_price < float(filters['min_price'])):
+                continue
+            if filters['max_price'] and (item.min_price is None or item.min_price > float(filters['max_price'])):
+                continue
+            filtered_items.append(item)
+        return filtered_items
+
+    def sort_items(self, items, sort_key, order):
+        if sort_key == 'name':
+            items.sort(key=lambda item: item.title.lower(), reverse=order=='descending')
+        elif sort_key == 'reviewers':
+            items.sort(key=lambda item: item.total_reviews, reverse=order == 'descending')
+        elif sort_key == 'rating':
+            items.sort(key=lambda item: item.average_rating, reverse=order == 'descending')
+        elif sort_key == 'price' and order == 'ascending':
+            items.sort(key=lambda item: item.min_price if item.min_price else 0xffffff)
+        elif sort_key == 'price' and order == 'descending':
+            items.sort(key=lambda item: item.max_price if item.max_price else 0, reverse=True)
+
     def get(self, request):
-        brand_filter = request.GET.get("brand", "")
-        rating_filter = request.GET.get("rating", 0)
-        min_price_filter = request.GET.get("min_price", "")
-        max_price_filter = request.GET.get("max_price", "")
-        items = []
-        for item in Item.objects.all():
-            if brand_filter and item.brand.name != brand_filter:
-                continue
-            if rating_filter and item.average_rating < float(rating_filter):
-                continue
-            if min_price_filter and (item.max_price is None or item.max_price < float(min_price_filter)):
-                continue
-            if max_price_filter and (max_price_filter is None or item.min_price > float(max_price_filter)):
-                continue
-            items.append(item)
+        # filter
+        filters = {
+            'keywords': request.GET.get("keywords", ""),
+            'brand': request.GET.get("brand", ""),
+            'carrier': request.GET.get("carrier", ""),
+            'min_price': request.GET.get("min_price", ""),
+            'max_price': request.GET.get("max_price", "")
+        }
+        items = self.filter_items(Item.objects.all(), filters)
+
+        # sort
+        sort = request.GET.get("sort", "")
+        sort_key, order = sort.split('-') if '-' in sort else ('', '')
+        self.sort_items(items, sort_key, order)
+
         ctx = {
             'brands': Brand.objects.all(),
+            'carriers': Carrier.objects.all(),
             'items': items,
-            'filters': {
-                'brand': brand_filter,
-                'rating': rating_filter,
-                'min_price': min_price_filter,
-                'max_price': max_price_filter
-            }
+            'filters': filters,
+            'sort': sort
         }
         return render(request, self.template, ctx)
 
